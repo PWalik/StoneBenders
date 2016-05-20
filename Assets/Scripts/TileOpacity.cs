de4b819e74280bc0923a585d505230712b39ddf3
Walik <dragonfire665@wp.pl>
2016-05-20 15:17:40 +0200

    lol

---
+++ Assets/Scripts/TileOpacity.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileOpacity : MonoBehaviour {
+	public float opacity = 1f;
+	// Use this for initialization
+	void Start () {
+		GetComponent<SpriteRenderer> ().color = new Color(GetComponent<SpriteRenderer> ().color.r,GetComponent<SpriteRenderer> ().color.g,GetComponent<SpriteRenderer> ().color.b,opacity);
+	}
+
+}
